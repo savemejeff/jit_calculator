74bc06bf6d9eb04da50d5215878041868c370f7a
Jiang Jingwei <jiang.jingwei@outlook.com>
2026-02-06 17:58:33 +0800

    add REPL mode, remove cal.sh script

--- main.c
+++ main.c
@@ -5,17 +5,41 @@
 
 typedef double (*run)();
 
-int main(int argc, const char **argv)
+void run_file(const char *filename)
 {
-    ASSERT(argc == 2 && "no input file");
-    char *source = read_entire_file(argv[1]);
+    char *source = read_entire_file(filename);
     Code *code = compile(source);
-    // for (size_t i = 0; i < code->count; i++) {
-    //     printf("%02x\n", code->items[i]);
-    // }
     run run = make_executable(code->items, code->count);
     double result = run();
     printf("%lf\n", result);
     free(source);
+}
+
+#define BUF_SIZE 1024
+void run_repl()
+{
+    char buf[BUF_SIZE];
+    while (1) {
+        if (fgets(buf, BUF_SIZE, stdin) == NULL) break;
+        Code *code = compile(buf);
+        run run = make_executable(code->items, code->count);
+        double result = run();
+        printf("%lf\n", result);
+    }
+}
+
+void help() {
+}
+
+int main(int argc, const char **argv)
+{
+    if (argc == 1) {
+        run_repl();
+    } else if (argc == 2) {
+        run_file(argv[1]);
+    } else {
+        help();
+        return 1;
+    }
     return 0;
 }
--- tests/runtests.sh
+++ tests/runtests.sh
@@ -5,7 +5,7 @@ cp -r ./../bin ./bin/
 for case in *.test; do
         touch "temp"
         while read -r line; do
-                printf "$line\n" | ./../cal.sh >> "temp"
+                printf "$line\n" | ./../bin/calculator >> "temp"
         done < "$case"
         cmp -s "temp" "${case%.*}.expect"
         if [ "$?" -eq "1" ]; then
